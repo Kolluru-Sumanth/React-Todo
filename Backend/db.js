450859b3aab14a4f8bc917d6717e38084f91507a
Kolluru Sumanth <kollurusumanth004@gmail.com>
2025-01-06 05:52:12 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose=require("mongoose");
-mongoose.connect(process.env.mongoURL);
+mongoose.connect(process.env.mongoUrl);
 
 const todoSchema= new mongoose.Schema({
     title: String,
